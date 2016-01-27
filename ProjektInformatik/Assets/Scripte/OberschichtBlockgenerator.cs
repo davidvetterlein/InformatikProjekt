a73d3cece91199becfbddd70723bf1e6803cb337
David Vetterlein <davidvtterlein@gmail.com>
2016-01-27 19:15:11 +0100

    no message

--- ProjektInformatik/Assets/Scripte/OberschichtBlockgenerator.cs
+++ ProjektInformatik/Assets/Scripte/OberschichtBlockgenerator.cs
@@ -4,15 +4,20 @@ using System.Collections;
 public class OberschichtBlockgenerator : MonoBehaviour {
     public GameObject Oberschicht;
     public Sprite Dirt;
+    public GameObject Grund;
+    int zufall = 0;
 	// Use this for initialization
 	void Start () {
-        if (gameObject.transform.position.x > PlayerPrefs.GetFloat("linkeWeltgroesse"))
+        zufall = Random.RandomRange(0, 10);
+        if (zufall > 8)
+        {
+            Instantiate(Grund, new Vector3(gameObject.transform.position.x -0.5f, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+            Instantiate(Oberschicht, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y +0.5f, gameObject.transform.position.z), Quaternion.identity);
+            gameObject.GetComponent<SpriteRenderer>().sprite = Dirt;
+        }
+        if (zufall <= 8 && gameObject.transform.position.x > PlayerPrefs.GetFloat("linkeWeltgroesse"))
         {
             Instantiate(Oberschicht, new Vector3(gameObject.transform.position.x - 0.5f, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
-        }
-        if (Random.RandomRange(0,10) > 8)
-        {
-            Instantiate(Oberschicht, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y +0.5f, gameObject.transform.position.z), Quaternion.identity);
         }
     }
 	
